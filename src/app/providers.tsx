0487a11c21f3a46f64184af4e11de6fd775a0160
Eda Akturk <edaakturk@Edas-MacBook-Pro.local>
2024-06-16 09:04:25 -0400

    Add onchain kit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState, type ReactNode } from 'react'
 import { WagmiProvider } from 'wagmi'
-
 import { config } from '@/wagmi'
+import '@coinbase/onchainkit/styles.css';
+
+import { baseSepolia } from 'viem/chains';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
 
 export function Providers(props: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient())
@@ -12,8 +15,11 @@ export function Providers(props: { children: ReactNode }) {
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
-                {props.children}
+                <OnchainKitProvider chain={baseSepolia}>
+                    {props.children}
+                </OnchainKitProvider>
             </QueryClientProvider>
         </WagmiProvider>
+
     )
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import { CartContext, CartItem } from "../cartContext";
 import { useContext } from "react";
 import { useAccount, useDisconnect } from 'wagmi'
-import { BlackCreateWalletButton, ConnectedWalletButton } from "../sw/BlackCreateWalletButton";
+
+import { Avatar, Name } from '@coinbase/onchainkit/identity';
+import { ConnectAccount } from '@coinbase/onchainkit/wallet';
 
 export function Header() {
-    const account = useAccount()
+    const { status, address } = useAccount()
     const { disconnect } = useDisconnect()
 
     const { cartProducts } = useContext(CartContext);
@@ -32,17 +34,30 @@ export function Header() {
                 <Link href="/about" className="hover:text-blue-600">About</Link>
             </nav>
             <div className="ml-auto flex items-center">
+                {status != 'connected' &&
+                    <div className="border-2 border-blue-500 rounded-full bold">
+                        <ConnectAccount />
+                    </div>
+                }
+                {status === 'connected' && (
+                    <button type="button" onClick={() => disconnect()} className="flex items-center">
+                        <Avatar address={address} />
+                        <div className="flex flex-col text-sm">
+                            <b>
+                                <Name address={address} />
+                            </b>
+                            <Name address={address} showAddress />
+                        </div>
+                    </button>
+                )}
                 <div className="dropdown dropdown-end">
-                    {account.status != 'connected' && <BlackCreateWalletButton />}
-                    {account.status === 'connected' && (
-                        <ConnectedWalletButton address={account.address} onDisconnect={disconnect} />
-                    )}
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                             <span className="badge badge-sm indicator-item">{totalItems}</span>
                         </div>
                     </div>
+
                     <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
                         <div className="card-body">
                             <span className="font-bold text-lg">{totalItems} Items</span>
